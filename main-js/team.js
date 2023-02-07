13ce47b985e17d8c35efbc0810afbb5a2b7ce72e
MichelepioBombai <michelebombai@icloud.com>
2023-02-07 10:08:57 +0100

    first-commit

---
+++ main-js/team.js
@@ -0,0 +1,50 @@
+// Wayne Barnett		Founder & CEO			wayne-barnett-founder-ceo.jpg
+// Angela Caroll		Chief Editor			angela-caroll-chief-editor.jpg
+// Walter Gordon		Office Manager			walter-gordon-office-manager.jpg
+// Angela Lopez		  Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada		Developer				scott-estrada-developer.jpg
+// Barbara Ramos		Graphic Designer		barbara-ramos-graphic-designer.jpg
+
+
+const teamMember =[
+  {
+    nome: "Wayne Barnett", 
+    ruolo: "Founder & CEO",
+    pic: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "Angela Caroll", 
+    ruolo: "Chief Editor",
+    pic: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    nome: "Walter Gordon", 
+    ruolo: "Office Manager",
+    pic: "walter-gordon-office-manager.jpg"
+  },
+  {
+    nome: "Angela Lopez", 
+    ruolo: "Social Media Manager",
+    pic: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    nome: "Scott Estrada", 
+    ruolo: "Developer",
+    pic: "scott-estrada-developer.jpg"
+  },
+  {
+    nome: "Barbara Ramos	", 
+    ruolo: "Graphic Designer",
+    pic: "barbara-ramos-graphic-designer.jpg"
+  }
+];
+
+for(const member of teamMember) {
+  console.log(member.nome);
+  console.log(member.ruolo);
+  console.log(member.pic);
+}
+
+
+
+  
